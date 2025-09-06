ad1997e749891eafb25470a65f9573d5b2c1b3eb
yeudaborodyanski <yeudaborodyanski@gmail.com>
2025-09-06 22:05:33 +0300

    [backend.config] fix sql default path

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret!")
     BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
-    DQLITE_PATH = os.getenv("DQLITE_PATH", "events.db")
+    DQLITE_PATH = os.getenv("DQLITE_PATH", "data/logs.db")
